80bd0ffe712f9f2fce2f4832c4539d0acff3e8a4
Syamsul MJ <syamsulmj94@gmail.com>
2025-04-14 23:14:40 +0800

    generate db url manually instead of declare in .env file

--- internal/database/database.go
+++ internal/database/database.go
@@ -19,7 +19,12 @@ func ConnectDB() {
 		log.Fatal("Error loading .env file")
 	}
 
-	dsn := os.Getenv("DATABASE_URL")
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+	)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("Failed to connect to database: %v", err)
