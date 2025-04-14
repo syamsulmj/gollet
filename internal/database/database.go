a2d597b537dc654ffa7b92816a66cb645c0599fd
Syamsul MJ <syamsulmj94@gmail.com>
2025-04-15 01:18:08 +0800

    create db on connect if doesn't exist

--- internal/database/database.go
+++ internal/database/database.go
@@ -19,13 +19,38 @@ func ConnectDB() {
 		log.Fatal("Error loading .env file")
 	}
 
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s sslmode=disable",
+	dsn := fmt.Sprintf("host=%s user=%s password=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+
+	dbName := os.Getenv("DB_NAME")
+
+	var exists bool
+	err = db.Raw("SELECT EXISTS(SELECT 1 FROM pg_database WHERE datname = ?)", dbName).Scan(&exists).Error
+	if err != nil {
+		log.Fatalf("Error checking database existence: %v", err)
+	}
+	if !exists {
+		// If the database does not exist, create it
+		CreateDatabase(db)
+	}
+
+	// Connect to the newly created database
+	dsn = fmt.Sprintf("host=%s user=%s password=%s dbname=%s sslmode=disable",
 		os.Getenv("DB_HOST"),
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
 		os.Getenv("DB_NAME"),
 	)
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("Failed to connect to database: %v", err)
 	}
@@ -42,4 +67,20 @@ func MigrateDB() {
 	}
 
 	fmt.Println("Database migration successful")
+}
+
+func CreateDatabase(db *gorm.DB) {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	dbName := os.Getenv("DB_NAME")
+
+	err = db.Exec(fmt.Sprintf("CREATE DATABASE %s", dbName)).Error
+	if err != nil {
+		log.Fatalf("Error creating database: %v", err)
+	}
+
+	fmt.Printf("Database %s created successfully\n", dbName)
 }
