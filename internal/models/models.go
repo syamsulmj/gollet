3394e45843187ab9ca6af872f80319dd0889686e
Syamsul MJ <syamsulmj94@gmail.com>
2025-04-15 11:17:07 +0800

    add ID for the table schemas

--- internal/models/models.go
+++ internal/models/models.go
@@ -3,26 +3,29 @@ package models
 import "gorm.io/gorm"
 
 type User struct {
-	gorm.Model
+	ID       uint   `gorm:"primaryKey"`
 	Email    string `gorm:"unique;not null"`
 	Name     string `gorm:"not null"`
 	Password string `gorm:"not null"`
 	Wallet   Wallet
+	gorm.Model
 }
 
 type Wallet struct {
-	gorm.Model
-	UserId   uint   `gorm:"unique;not null"`
+	ID       uint   `gorm:"primaryKey"`
+	UserID   uint   `gorm:"unique;not null"`
 	Balance  uint   `gorm:"default:0"`  // Amount in cents
 	Currency string `gorm:"not null"`   // Currency code (e.g., USD, EUR)
 	Metadata string `gorm:"type:jsonb"` // Store extra info as JSON
+	gorm.Model
 }
 
 type Transaction struct {
-	gorm.Model
-	UserId          uint   `gorm:"not null"`
+	ID              uint   `gorm:"primaryKey"`
+	UserID          uint   `gorm:"not null"`
 	TransactionType string `gorm:"not null"`   // deposit, withdraw, transfer_sent, transfer_received
 	Amount          uint   `gorm:"not null"`   // Amount in cents
 	Currency        string `gorm:"not null"`   // Currency code (e.g., USD, EUR)
 	Metadata        string `gorm:"type:jsonb"` // Store extra info as JSON
+	gorm.Model
 }
